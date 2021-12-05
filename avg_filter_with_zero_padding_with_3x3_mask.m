cb24f1975beea9e9ac7e736fef4ef77753ab8b44
priyabrata <naskar.priyabrata66@gmail.com>
2021-12-05 07:47:10 +0530

    "initial filters with 3x3 mask"

---
+++ avg_filter_with_zero_padding_with_3x3_mask.m
@@ -0,0 +1,51 @@
+I = imread('C:\Users\priya\Downloads\test.png');
+%imshow(I);
+size(I);
+grayImage = rgb2gray(I);
+oldImageSize = size(grayImage);
+
+%applying padding zero
+newImage = zeros(oldImageSize(1)+4, oldImageSize(2)+4);
+newImage = uint8(newImage);
+mLength = size(newImage);
+k=1;
+l=1;
+for i=1:oldImageSize(1)
+    for j=1:oldImageSize(2)
+        a = grayImage(i,j);
+        newImage(i+2,j+2) = a;
+        %l = l+1;
+    end
+    %k = k+1;
+end
+
+%plotting original gray image with padding
+subplot(1,2,1)
+imshow(newImage)
+title('Original Image')
+
+%empty matrix for filtered image
+filteredImage = uint8(zeros(oldImageSize(1)+4, oldImageSize(2)+4));
+
+%average filter
+for i=3:oldImageSize(1)+2
+    for j=3:oldImageSize(2)+2
+        t = [newImage(i-1,j+1),newImage(i,j+1),newImage(i+1,j+1),newImage(i-1,j),newImage(i,j),newImage(i+1,j),newImage(i-1,j-1),newImage(i,j-1),newImage(i+1,j-1)];
+        total = double(0);
+        val=double(0);
+            for index=1:9
+                val = double(t(index));
+                total = double(total + val);
+            end
+
+        avg=total/9;
+        filteredImage(i,j) = (uint8(avg));
+        %l = l+1;
+    end
+    %k = k+1;
+end
+
+%plotting the filtered image
+subplot(1,2,2)
+imshow(filteredImage)
+title('Average Filtered Image')
---
+++ max_filter_with_zero_padding_with_3x3_matrix.m
@@ -0,0 +1,45 @@
+I = imread('C:\Users\priya\Downloads\test.png');
+%imshow(I);
+size(I);
+grayImage = rgb2gray(I);
+oldImageSize = size(grayImage);
+
+%applying padding zero
+newImage = zeros(oldImageSize(1)+4, oldImageSize(2)+4);
+newImage = uint8(newImage);
+mLength = size(newImage);
+k=1;
+l=1;
+for i=1:oldImageSize(1)
+    for j=1:oldImageSize(2)
+        a = grayImage(i,j);
+        newImage(i+2,j+2) = a;
+        %l = l+1;
+    end
+    %k = k+1;
+end
+
+%plotting original gray image with padding
+subplot(1,2,1)
+imshow(newImage)
+title('Original Image')
+
+%empty matrix for filtered image
+filteredImage = uint8(zeros(oldImageSize(1)+4, oldImageSize(2)+4));
+
+%max filter
+for i=3:oldImageSize(1)+2
+    for j=3:oldImageSize(2)+2
+        t = [newImage(i-1,j+1),newImage(i,j+1),newImage(i+1,j+1),newImage(i-1,j),newImage(i,j),newImage(i+1,j),newImage(i-1,j-1),newImage(i,j-1),newImage(i+1,j-1)];
+        val=max(t);
+        
+        filteredImage(i,j) = (uint8(val));
+        %l = l+1;
+    end
+    %k = k+1;
+end
+
+%plotting the filtered image
+subplot(1,2,2)
+imshow(filteredImage)
+title('Max Filtered Image')
